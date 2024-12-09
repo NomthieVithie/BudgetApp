bece3d83dd31af77c56eea440974b35333a308ec
https://github.com/NomthieVithie/MyRunApp <toodeelicious@gmail.com>
2024-12-09 15:59:29 +0000

    removed 2 buttons

--- app/src/main/java/com/example/preparationapp/ExamAdapter.kt
+++ app/src/main/java/com/example/preparationapp/ExamAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 //Nomthandazo Ndlovu st10432979
 
